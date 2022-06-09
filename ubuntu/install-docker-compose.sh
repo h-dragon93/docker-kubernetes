e234d44ba4ade75d50b2bf1330132e928f6350a6
Han-dragon93 <gksdyd4238@naver.com>
2022-06-10 00:15:50 +0900

    Create install-docker-compose.sh

---
+++ ubuntu/install-docker-compose.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+## INFO: https://docs.docker.com/compose/install/
+## 도커 컴포즈 설치 쉘 스크립트
+
+set -euf -o pipefail
+
+DOCKER_COMPOSE_VERSION=v2.1.1
+
+# Download and install
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
